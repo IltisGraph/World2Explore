8400e4bb2cfb51dade3ac1a68245a34b1f18e312
IltisGraph <105117627+IltisGraph@users.noreply.github.com>
2022-07-08 16:43:37 +0000

    Added green background to the Game loop in JS.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+class game{
+	
+	drawBackground(ctx){
+		ctx.fillStyle = "#0f0";
+		ctx.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+	}
+}
+
+
+const ctx = document.getElementById("GameScreen").getContext("2d");
+
+const GAME_WIDTH = 2000;
+const GAME_HEIGHT = 1000;
+
+const Game = new game();
+
+
+let lastTime = 0;
+
+function GameLoop(dt){
+	let deltaTime = dt - lastTime;
+	lastTime = dt;
+	Game.drawBackground(ctx);
+
+
+
+
+
+	requestAnimationFrame(GameLoop);
+}
+
+GameLoop(1);
